68c1189b5d35f9717ed4a73637000318ff0f01be
Anthony Pennacchio <mrpennacchio@gmail.com>
2017-02-23 09:07:00 -0500

    timer starts and resets

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,36 @@
 // Initialize Firebase Module
 var app = angular.module("BlocTime", ["firebase"]);
+
+//new timer session controller
+app.controller('newSessionCtrl', ['$scope', '$interval', function($scope, $interval){
+
+    $scope.time = 1500;
+    var success;
+    $scope.startTimer = function(){
+      // don't execute function if it is already running
+      if (success === true){
+        return;
+      }
+      // start countdown function
+      $scope.countDown = $interval(function(){
+        if ($scope.time == 0){ //stop timer when it reaches 0
+          $scope.time = 0;
+        } else {
+          $scope.time--;
+          success = true;
+        }
+      },1000);
+    };
+
+    // reset timer
+    $scope.resetTimer = function(){
+      $scope.time = 1500;
+    }
+}]);
+
+// convert seconds to minutes
+app.filter('secondsToMinutes', [function(){
+  return function(time) {
+    return new Date(1970, 0 ,1).setSeconds(time);
+  }
+}])
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,11 +1,36 @@
 // Initialize Firebase Module
 var app = angular.module("BlocTime", ["firebase"]);
 
-app.controller("SampleCtrl", function($scope, $firebaseObject) {
-  var ref = firebase.database().ref().child("data");
-  // download the data into a local object
-  var syncObject = $firebaseObject(ref);
-  // synchronize the object with a three-way data binding
-  // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
-});
+//new timer session controller
+app.controller('newSessionCtrl', ['$scope', '$interval', function($scope, $interval){
+
+    $scope.time = 1500;
+    var success;
+    $scope.startTimer = function(){
+      // don't execute function if it is already running
+      if (success === true){
+        return;
+      }
+      // start countdown function
+      $scope.countDown = $interval(function(){
+        if ($scope.time == 0){ //stop timer when it reaches 0
+          $scope.time = 0;
+        } else {
+          $scope.time--;
+          success = true;
+        }
+      },1000);
+    };
+
+    // reset timer
+    $scope.resetTimer = function(){
+      $scope.time = 1500;
+    }
+}]);
+
+// convert seconds to minutes
+app.filter('secondsToMinutes', [function(){
+  return function(time) {
+    return new Date(1970, 0 ,1).setSeconds(time);
+  }
+}])
