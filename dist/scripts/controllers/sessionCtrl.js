c368d8a9f3d279793db94d7e0a5c91eeeccb55f7
Anthony Pennacchio <mrpennacchio@gmail.com>
2017-02-27 13:26:14 -0500

    added sounds

--- dist/scripts/controllers/sessionCtrl.js
+++ dist/scripts/controllers/sessionCtrl.js
@@ -2,13 +2,24 @@ angular.module('BlocTime')
     .controller('sessionCtrl', ['$scope', '$interval', 'TIMER', function($scope, $interval, TIMER){
 
       $scope.onBreak = false; //
-      $scope.time = 1500; // default time onload
+      $scope.time = 150; // default time onload
       // var breakTime = 300;
       // var workTime = 150;
       $scope.message = "Start"
       $scope.resetMessage = "Reset"
       $scope.breakMessage = "Start Break"
-      var sessionCount = 0;
+
+      var mySound = new buzz.sound("/assets/ding.mp3", {
+	       preload: true
+      });
+
+    //  $scope.$watch('sound', function(){
+	  //     if ($scope.time === 0){
+	  //        mySound();
+	  //       }
+    //  });
+
+
 
       $scope.startTimer = function(){
         // don't execute function if it is already running
@@ -27,19 +38,13 @@ angular.module('BlocTime')
 
           if ($scope.time === 0){ //stop timer when it reaches 0
             $scope.stopTimer();
+            mySound.play();
             $scope.time = TIMER.BREAK_TIME;
-            alert("Time for a break");
+           // alert("Time for a break");
             $scope.onBreak = true;
             $scope.timerRunning = false;
-            sessionCount += 1;
-	          console.log(sessionCount);
-            if (sessionCount % 4 == 0){
-              $scope.time = TIMER.LONG_BREAK;
-              sessionCount = 0;
-              alert("Time for a long break");
-            };
           };
-        },1000);
+        },1);
       };
 
       $scope.startBreakTimer = function(){
@@ -56,18 +61,17 @@ angular.module('BlocTime')
           $scope.timerRunning = true;
           $scope.onBreak = true;
           $scope.breakMessage = "Its Break Time"
-          console.log(sessionCount);
-
 
           if ($scope.time === 0){ //stop timer when it reaches 0
             $scope.stopBreakTimer();
+            mySound.play();
             $scope.time = TIMER.WORK_TIME;
-            alert("Break Done")
+           // alert("Break Done")
             $scope.onBreak = false;
             $scope.timerRunning = false;
             $scope.message = "Back to Work"
           };
-        },1000);
+        },1);
       };
 
       // stop work session
